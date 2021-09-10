6332a8ba2a9650442384b1d391ea34b9884ebc7d
lekoal <coreglus@gmail.com>
2021-09-10 11:14:17 +0300

    my arraylist add/delete/search adding

--- src/ru/geekbrains/java_algoritms_homework_2/Main.java
+++ src/ru/geekbrains/java_algoritms_homework_2/Main.java
@@ -3,7 +3,20 @@ package ru.geekbrains.java_algoritms_homework_2;
 public class Main {
 
     public static void main(String[] args) {
-	MyArrayList myArrayList = new MyArrayList(100);
-	myArrayList.print();
+        MyArrayList myArrayList = new MyArrayList(10);
+        myArrayList.print();
+        myArrayList.add(9, 55);
+        myArrayList.print();
+        myArrayList.add(16);
+        myArrayList.print();
+        myArrayList.deleteIndex(10);
+        myArrayList.print();
+        myArrayList.deleteAllValue(314);
+        myArrayList.print();
+        myArrayList.deleteValue(16);
+        myArrayList.print();
+        System.out.println(myArrayList.searchByIndex(4));
+        System.out.println(myArrayList.searchByValue(55));
+
     }
 }
--- src/ru/geekbrains/java_algoritms_homework_2/MyArrayList.java
+++ src/ru/geekbrains/java_algoritms_homework_2/MyArrayList.java
@@ -1,5 +1,6 @@
 package ru.geekbrains.java_algoritms_homework_2;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import java.util.ArrayList;
@@ -16,6 +17,39 @@ public class MyArrayList {
         for (int i = 0; i < capacity; i++) {
             myArrayList.add(random.nextInt(1000));
         }
+    }
+
+    public void add(int index, int value) {
+        if (index <= 0) throw new IllegalArgumentException("index cant be <= 0");
+        myArrayList.add(index, value);
+    }
+
+    public void add(int value) {
+        myArrayList.add(value);
+    }
+
+    public void deleteIndex(int index) {
+        if (index <= 0) throw new IllegalArgumentException("index cant be <= 0");
+        myArrayList.remove(index);
+    }
+
+    public void deleteAllValue(int value) {
+        myArrayList.removeAll(Collections.singleton(value));
+    }
+
+    public void deleteValue(int value) {
+        myArrayList.remove((Object) value);
+    }
+
+    public int searchByIndex(int index) {
+        if (index < 0 || index > myArrayList.size())
+            throw new IllegalArgumentException("index cant be < 0 or > array size");
+        return myArrayList.get(index);
+    }
+
+    public int searchByValue(int value) {
+        if (myArrayList.contains(value)) return myArrayList.indexOf(value);
+        return -1;
     }
 
     public void print() {
